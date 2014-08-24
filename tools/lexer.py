dbd03d8fd493a64806740e9f8bde94b14aa381cd
Marek Marecki <marekjm at ozro dot pw>
2014-08-24 22:11:14 +0200

    Furter cleanup

--- tools/lexer.py
+++ tools/lexer.py
@@ -27,7 +27,7 @@ USAGE:
 
 BUGS:
     Any bugs should be reported on Tartak's github page.
-    This tool operates on Tartak version {tartak_version}.
+    This tool operates on Tartak version '{tartak_version}'.
 """
 
 import glob
@@ -49,58 +49,42 @@ except ImportError as e:
 args = sys.argv[1:]
 
 if len(args) < 2 or len(args) > 3:
-    if args and args[0] != '--help':
+    if not (args and args[0] == '--help'):
         print('fatal: invalid number of operands: expected between 2 and 3 but got {0}'.format(len(args)))
         exit(1)
 
 if args[0] == '--help':
-    print(__help__.format(tartak_version=VERSION))
+    print(__doc__.format(tartak_version=VERSION))
     exit(0)
 
 
-LEXER_RULES = (args[0] if os.path.isfile(args[0]) else None)
-PATH = (args[1] if os.path.isfile(args[0]) else None)
+LEXER_RULES = args[0]
+PATH = args[1]
 OUTPUT = (args[2] if len(args) == 3 else 'a.tokens')
 
-if path is None:
-    print('"{0}" does not point to a file'.format(args[0]))
-    exit(2)
+if not os.path.isfile(LEXER_RULES):
+    print('fatal: {0} does not point to a file'.format(repr(LEXER_RULES)))
+    exit(3)
 
-ifstream = open(path, 'r')
-text = ifstream.read()
-ifstream.close()
+if not os.path.isfile(PATH):
+    print('fatal: {0} does not point to a file'.format(repr(PATH)))
+    exit(3)
 
-
-KEYWORDS = [
-        'import',
-        'using',
-
-        'namespace',
-        'class',
-        'function',
-
-        'int',
-        'bool',
-        'float',
-        ]
-
-GRAMMAR_CHARS = [
-        ('lparen', '('),
-        ('rparen', ')'),
-        ('eos', ';'),
-        ('stream-out', '<<'),
-        ('stream-in', '>>'),
-        ]
+try:
+    ofstream = open(OUTPUT, 'w')
+    ofstream.write('')
+    ofstream.close()
+    err = None
+except Exception as e:
+    err = e
+finally:
+    if err is not None:
+        print('fatal: cannot create output file: {0}'.format(OUTPUT))
+        exit(4)
 
 
-tknzr = source.tokenizer.Tokenizer()
-
-for k in KEYWORDS:
-    if not k: continue
-    tknzr.addRule(name='keyword-{0}'.format(k), group='keyword', rule=k, type='string')
-
-for name, rule in GRAMMAR_CHARS:
-    tknzr.addRule(name=name, group='grammar_chars', rule=rule, type='string')
+# TODO: fix further code
+exit(99)
 
 tknzr.addRule(name='name', rule='[a-zA-Z_][a-zA-Z0-9_]*', type='regex')
 tknzr.addRule(name='comment-inline', rule='//.*(\n|$)', type='regex')
